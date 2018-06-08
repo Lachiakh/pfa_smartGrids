28e4d6568939849bb7e490f04c164cc8494516e9
IDOUDAOUD <med.idoudaoud@gmail.com>
2018-06-08 04:32:17 +0000

    version beta

--- index.py
+++ index.py
@@ -3,8 +3,11 @@ import os
 import pandas as pd
 import xlrd
 import seaborn as sns
+from pylab import figure
 import shutil
 import pathlib
+import numpy as np
+import random
 
 app = Flask(__name__)
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -13,12 +16,76 @@ app.secret_key='abcd'
 @app.route('/')
 def index():
         session.clear()
+        shutil.rmtree('static/img')
+        os.makedirs('static/img', exist_ok=False)
         return render_template('index.html')
 
 
-@app.route('/avg/<sheet>')
-def avg(sheet=None):
-    return render_template('avg.html')
+
+
+
+@app.route('/pivot/<sheet>', methods=['POST','GET'])
+def pivot(sheet=None,na=None):
+    argument1 = request.form.get("argument1")
+    argument2 = request.form.get("argument2")
+    argument3 = request.form.get("argument3")
+    if (argument1 is not None):
+        try:
+            data=pivot_count(sheet_selection(sheet),argument1,argument2,argument3)
+            data.to_html('templates/table_pivot.html')
+            sns.set_style("whitegrid")
+            ax= sns.heatmap(data, annot=True, fmt='g')
+            ax.set(xlabel=' Total Customer count')
+            #os.remove('static/img/pivot.png')
+            #os.makedirs('static/img', exist_ok=False)
+            na = random.randint(1, 1000)
+
+            ax.figure.savefig('static/img/'+str(na)+'.png')
+        except ValueError :
+            return "Error: this graph can't be displayed <a href='/avg/" + sheet + "' > go to your page </a>"
+    return render_template('pivot.html',na=na,sheet=sheet,sheet_names=session['sheet_names'],argument1=argument1,argument2=argument2,argument3=argument3,columns=column_name(sheet))
+
+@app.route('/avg/<sheet>', methods=['POST','GET'])
+def avg(sheet=None,na=None):
+    argument1=None
+    argument2=None
+    argument1=request.form.get("argument1")
+    argument2=request.form.get("argument2")
+    if (argument1 is not None and argument2 is not None):
+        try:
+
+            data=agg_avg(sheet_selection(sheet), argument1, argument2)
+            data.to_html('templates/table_avg.html')
+            sns.set_style("whitegrid")
+            na = random.randint(1, 1000)
+            sns.barplot( x='Avg '+ argument2, y=argument1, data=data,palette="Blues_d").figure.savefig('static/img/' + str(na)+ '.png')
+            #ax.set(xlabel='\n average of '+ argument2 +' depends on ' + argument1)
+            #shutil.rmtree('static/img')
+            #os.makedirs('static/img', exist_ok=False)
+
+        except ValueError :
+            return "Error: this graph can't be displayed <a href='/avg/" + sheet + "' > go to your page </a>"
+    return render_template('avg.html',na=na,sheet=sheet,sheet_names=session['sheet_names'],argument1=argument1,argument2=argument2,columns=column_name(sheet))
+
+@app.route('/regression/<sheet>', methods=['POST','GET'])
+def regression(sheet=None,na=None):
+    argument1=None
+    argument2=None
+    argument1=request.form.get("argument1")
+    argument2=request.form.get("argument2")
+    if (argument1 is not None and argument2 is not None):
+        #try:
+
+            data=sheet_selection(sheet)
+            na = random.randint(1, 1000)
+            sns.pairplot(data[[argument1,argument2]], hue=argument1, size=3).savefig('static/img/' + str(na)+ '.png')
+            #ax.set(xlabel='\n average of '+ argument2 +' depends on ' + argument1)
+            #shutil.rmtree('static/img')
+            #os.makedirs('static/img', exist_ok=False)heet_selection(sheet)
+
+        #except ValueError :
+         #   return "Error: this graph can't be displayed <a href='/avg/" + sheet + "' > go to your page </a>"
+    return render_template('regression.html',na=na,sheet=sheet,sheet_names=session['sheet_names'],argument1=argument1,argument2=argument2,columns=column_name(sheet))
 
 
 @app.route('/<sheet>/<column>/<column1>',methods=['POST','GET'])
@@ -33,14 +100,18 @@ def explore(sheet=None,argument=None,column=None,column1=None):
     if sheet is not None:
         sheet_selection(sheet).to_html('templates/table_sheet.html')
 
+
     if column is not None and column1 is None:
-        agg_count(sheet_selection(sheet), column).to_html('templates/table_count.html')
-        sns.set_style("whitegrid")
-        ax = sns.barplot( y='Count', x=column,data=agg_count(sheet_selection(sheet), column).head())
-        ax.set(xlabel='\n Number of '+column )
-        shutil.rmtree('static/img')
-        os.makedirs('static/img', exist_ok=True)
-        ax.figure.savefig('static/img/'+column+'.png')
+        try:
+            agg_count(sheet_selection(sheet), column).to_html('templates/table_count.html')
+            sns.set_style("whitegrid")
+            ax = sns.barplot( y='Count', x=column,data=agg_count(sheet_selection(sheet), column).head())
+            ax.set(xlabel='\n Number of '+column )
+            shutil.rmtree('static/img')
+            os.makedirs('static/img', exist_ok=True)
+            ax.figure.savefig('static/img/'+column+'.png')
+        except ValueError :
+            return "Error: this graph can't be displayed <a href='" + "/" + sheet + "/" + column + "' > go to your page </a>"
     if argument is not None:
         try:
             my_data = sheet_selection(sheet)
@@ -72,10 +143,12 @@ def explore(sheet=None,argument=None,column=None,column1=None):
                     return render_template('index.html',echec="Error",msg="please, you should upload a file")
                 session['filename']=filename
     ##############################################
+
     header_smart = pd.read_excel(session['filename'], header=None, sheet_name=0, usecols=1)
     header_smart.head(7).to_html('templates/header_smart.html')
 
-        ################
+            ################
+
     xlsx = pd.ExcelFile(session['filename'])
     smart_sheets = []
     i = 0
@@ -83,7 +156,8 @@ def explore(sheet=None,argument=None,column=None,column1=None):
     for isheet in sheet_names:
         smart_sheets.append(pd.read_excel(session['filename'], sheet_name=i, skiprows=10))
         i = i + 1
-
+    session['sheet_names'] =  sheet_names
+    session['first']=1
 
     ################################
 
@@ -216,6 +290,15 @@ def agg_avg(df, group_field, calc_field):
     #grouped = grouped.sort(calc_field, ascending = False)
     grouped.columns = [group_field, 'Avg ' + str(calc_field)]
     return grouped
+
+
+def pivot_count(df, rows, columns, calc_field):
+    df_pivot = df.pivot_table(values=calc_field,
+                              index=rows,
+                              columns=columns,
+                              aggfunc=np.size
+                             ).dropna(axis=0, how='all')
+    return df_pivot
 
 if __name__ == '__main__':
     app.run(debug=True)
